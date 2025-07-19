8fd11a274221033ee97994a8631475ea0a5f7f70
kzfly1 <qichenxu47@gmail.com>
2025-07-19 23:12:37 +1000

    feat(backend): [SCRUM-56] implement Repository Layer and GetUserByIdAsync method

---
+++ HelloCity.Repository/UserRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelloCity.Api.Data;
+using HelloCity.IRepository;
+using HelloCity.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloCity.Repository
+{
+    public class UserRepository: IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Get user by userId
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
+        public async Task<Users?> GetUserByIdAsync(Guid userId)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(u => u.UserId == userId);
+        }
+    }
+}
