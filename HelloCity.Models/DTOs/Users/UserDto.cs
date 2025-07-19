9e4f0b9e07cf72dc054142f1faf1a8458c3ebe18
kzfly1 <qichenxu47@gmail.com>
2025-07-19 21:29:23 +1000

    feat(backend): [SCRUM-56] add UserDto for user profile response

---
+++ HelloCity.Models/DTOs/Users/UserDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelloCity.Models.Enums;
+
+namespace HelloCity.Models.DTOs.Users
+{
+    public class UserDto
+    {
+        public Guid UserId { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set;}
+        public required string Avatar { get; set; }
+        public Gender Gender { get; set; }
+        public string? Nationality { get; set; }
+        public string? City { get; set; }
+        public string? University { get; set; }
+        public string? Major { get; set; }
+        public PreferredLanguage PreferredLanguage { get; set; }
+        public DateTime LastJoinDate { get; set; }
+    }
+}
