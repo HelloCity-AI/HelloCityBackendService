b9ab089cc70a432f345567258b8142751fc54396
kzfly1 <qichenxu47@gmail.com>
2025-07-20 23:24:00 +1000

    feat(utils): [SCRUM-56] add DateTimeHelper and unit tests for GetSydneyNow/ConvertUtc

---
+++ HelloCity.Tests/Utils/DateTimeHelperTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using HelloCity.Models.Utils;
+using Xunit;
+
+namespace HelloCity.Tests.Utils
+{
+    public class DateTimeHelperTests
+    {
+        /// <summary>
+        /// Tests whether GetUtcNow returns a DateTime that is within 2 seconds of the current UTC time.
+        /// </summary>
+        [Fact]
+        public void GetUtcNow_ShouldReturnNearNow()
+        {
+            var systemNow = DateTime.UtcNow;
+            var helperNow = DateTimeHelper.GetUtcNow();
+
+            Assert.True(Math.Abs((helperNow - systemNow).TotalSeconds) < 2);
+        }
+
+        /// <summary>
+        /// Test whether GetSydneyNow returns a DateTime that is within an expected offset from UTC.
+        /// </summary>
+        [Fact]
+        public void GetSydneyNow_ShouldBeWithinExceptedOffset()
+        {
+            var utc = DateTime.UtcNow;
+            var sydney = DateTimeHelper.GetSydneyNow();
+            var utcOffset = sydney - utc;
+
+            Assert.InRange(utcOffset.TotalHours, 8, 11);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [Fact]
+        public void ConvertUtcToSydney_ShouldReturnCorrectSydneyTime()
+        {
+            var testUtc = new DateTime(2025, 7, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            var sydneyTime = DateTimeHelper.ConvertUtcToSydney(testUtc);
+
+            Assert.Equal(2025, sydneyTime.Year);
+            Assert.Equal(7, sydneyTime.Month);
+        }
+    }
+}
