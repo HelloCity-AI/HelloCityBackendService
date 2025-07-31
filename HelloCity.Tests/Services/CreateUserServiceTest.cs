8880c022490915a2ba40867bf8d87eb6addd4613
kzfly1 <qichenxu47@gmail.com>
2025-07-31 14:44:34 +1000

    test(backend): [SCRUM-67] refactor CreateUserServiceTest and  remove AutoMapper dependency

--- HelloCity.Tests/Services/CreateUserServiceTest.cs
+++ HelloCity.Tests/Services/CreateUserServiceTest.cs
@@ -1,35 +1,24 @@
 using Xunit;
 using Moq;
 using FluentAssertions;
-using AutoMapper;
 using HelloCity.Services;
 using HelloCity.IRepository;
 using HelloCity.Models.Entities;
-using HelloCity.Api.DTOs.Users;
 using HelloCity.Models.Enums;
 using System;
 using System.Threading.Tasks;
-using HelloCity.Tests.Helpers;
-using Microsoft.Extensions.Logging;
+
 
 
 public class CreateUserServiceTest
 {
     private readonly Mock<IUserRepository> _userRepositoryMock;
-    private readonly IMapper _mapper;
     private readonly UserService _userService;
 
     public CreateUserServiceTest()
     {
         _userRepositoryMock = new Mock<IUserRepository>();
-
-        var config = new MapperConfiguration(cfg =>
-        {
-            cfg.AddProfile(new UserTestProfile());
-        }, new LoggerFactory());
-
-        _mapper = config.CreateMapper();
-        _userService = new UserService(_userRepositoryMock.Object, _mapper);
+        _userService = new UserService(_userRepositoryMock.Object);
     }
 
     [Fact]
@@ -37,7 +26,7 @@ public class CreateUserServiceTest
     {
         // Arrange
 
-        var dto = new CreateUserDto
+        var user = new Users
         {
             Username = "john_dev",
             Email = "john@example.com",
@@ -53,16 +42,14 @@ public class CreateUserServiceTest
         _userRepositoryMock.Setup(r => r.AddUserAsync(It.IsAny<Users>()))
         .ReturnsAsync((Users u) => u);
 
-        var _userService = new UserService(_userRepositoryMock.Object, _mapper);
-
         // Act
-        var result = await _userService.CreateUserAsync(dto);
+        var result = await _userService.CreateUserAsync(user);
 
         // Assert
         result.Should().NotBeNull();
-        result.Username.Should().Be(dto.Username);
-        result.Email.Should().Be(dto.Email);
-        result.Gender.Should().Be(dto.Gender);
+        result.Username.Should().Be(user.Username);
+        result.Email.Should().Be(user.Email);
+        result.Gender.Should().Be(user.Gender);
         _userRepositoryMock.Verify(r => r.AddUserAsync(It.IsAny<Users>()), Times.Once);
     }
 }
