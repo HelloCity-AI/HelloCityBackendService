909136d7776a5cdd910a0db6f7de8a75105a48f1
kzfly1 <qichenxu47@gmail.com>
2025-07-31 11:37:05 +1000

    refactor(backend): [SCRUM-67] update IUserService interface to remove DTO dependencies

--- HelloCity.IServices/IUserService.cs
+++ HelloCity.IServices/IUserService.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HelloCity.Api.DTOs.Users;
+﻿using HelloCity.Models.Entities;
 
 namespace HelloCity.IServices
 {
     public interface IUserService
     {
-        Task<UserDto?> GetUserProfileAsync(Guid userId);
-        Task<UserDto> CreateUserAsync(CreateUserDto dto);
+        Task<Users?> GetUserProfileAsync(Guid userId);
+        Task<Users> CreateUserAsync(Users user);
     }
 }
