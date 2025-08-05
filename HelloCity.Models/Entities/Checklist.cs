97e91ec4c6003850ad32a3e21c7d075c7b849d8b
Paul Zhang <hzhang526@gmail.com>
2025-08-05 14:57:20 +1000

    feat(backend): [SCRUM-63] Create bi-directional relationship between User and ChecklistItem

--- HelloCity.Models/Entities/Checklist.cs
+++ HelloCity.Models/Entities/Checklist.cs
@@ -1,20 +1,16 @@
-using HelloCity.Models.Enums;
 using ImportanceEnum = HelloCity.Models.Enums.ImportanceLevel;
-
 
 namespace HelloCity.Models.Entities
 {
-  public class ChecklistItems
+  public class ChecklistItem
   {
     public Guid ChecklistItemId { get; set; } = Guid.NewGuid();
-    /*Can change to integer ID depending on implementation*/
-    // Foreign key
-    public Guid UserId { get; set; }
+    public Guid UserOwnerId { get; set; }
+    public required Users UserOwner { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public bool IsComplete { get; set; } = false;
-    public ImportanceLevel Importance { get; set; } = ImportanceEnum.Low;
-
-
+    public ImportanceEnum Importance { get; set; } = ImportanceEnum.Low;
   }
 }
+
