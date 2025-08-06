5972fc0334b2f5a3824001e51693c5e10dc35868
Paul Zhang <hzhang526@gmail.com>
2025-08-07 00:27:59 +1000

    feat(db): [SCRUM-79] Create dto/profile for checklistItem post method

--- HelloCity.Api/DTOs/ChecklistItems/ChecklistItemDto.cs
+++ HelloCity.Api/DTOs/ChecklistItems/ChecklistItemDto.cs
@@ -1,14 +1,15 @@
-
 using ImportanceEnum = HelloCity.Models.Enums.ImportanceLevel;
 
 namespace HelloCity.Api.DTOs.ChecklistItem
 {
-  public class CreateChecklistItemDto
+  public class ChecklistItemDto
   {
-    public Guid UserOwnerId { get; set; }   // required
+    public Guid ChecklistItemId { get; set; }
+    public Guid UserOwnerId { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public bool IsComplete { get; set; } = false;
     public ImportanceEnum Importance { get; set; } = ImportanceEnum.Low;
+
   }
 }
