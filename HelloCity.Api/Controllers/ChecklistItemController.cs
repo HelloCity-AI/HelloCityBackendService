75c94c872034e4ddb5aa83071647be25af534427
Paul Zhang <hzhang526@gmail.com>
2025-08-08 17:05:17 +1000

    Fix(backend): [SCRUM-79] Add KeyNotFoundException for not finding User, and 500 for other exceptions

--- HelloCity.Api/Controllers/ChecklistItemController.cs
+++ HelloCity.Api/Controllers/ChecklistItemController.cs
@@ -3,13 +3,12 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using HelloCity.Models.Entities;
 using AutoMapper;
-using HelloCity.Api.DTOs.Users;
 using HelloCity.Api.DTOs.ChecklistItem;
 using System.Diagnostics;
 
 namespace HelloCity.Api.Controllers
 {
-  [Route("/api/checklist-item")]
+  [Route("api/users/{userId}/checklist-item")]
   [ApiController]
   public class ChecklistItemController : ControllerBase
   {
@@ -18,31 +17,35 @@ namespace HelloCity.Api.Controllers
       private readonly ILogger<ChecklistItemController> _logger;
 
       public ChecklistItemController(
-          IChecklistItemService checklistItemService, 
-          IMapper mapper,
-          ILogger<ChecklistItemController> logger)
+        IChecklistItemService checklistItemService, 
+        IMapper mapper,
+        ILogger<ChecklistItemController> logger)
       {
-          _checklistItemService = checklistItemService;
-          _mapper = mapper;
-          _logger = logger;
+        _checklistItemService = checklistItemService;
+        _mapper = mapper;
+        _logger = logger;
       }
 
-    [HttpPost("{id}")]
-    public async Task<ActionResult<ChecklistItem>> CreateChecklistItem(Guid id, CreateChecklistItemDto newChecklistItemDto)
+    [HttpPost]
+    public async Task<ActionResult<ChecklistItem>> CreateChecklistItem(Guid userId, CreateChecklistItemDto newChecklistItemDto)
     {
-      _logger.LogInformation("Creating checklist for User with ID: {id}", id);
-
+      _logger.LogInformation("Creating checklist for User with ID: {userId}", userId);
       try
       {
         var newChecklistItem = _mapper.Map<ChecklistItem>(newChecklistItemDto);
-        var savedChecklistItem = await _checklistItemService.AddChecklistItemAsync(id, newChecklistItem);
+        var savedChecklistItem = await _checklistItemService.AddChecklistItemAsync(userId, newChecklistItem);
         var checklistItemDto = _mapper.Map<ChecklistItemDto>(savedChecklistItem);
         return Created("/api/checklist-item/" + checklistItemDto.ChecklistItemId, checklistItemDto);
       }
+      catch (KeyNotFoundException ex)
+      {
+        _logger.LogWarning(ex, "User not found with ID: {userId}", userId);
+        return NotFound("User not found");
+      }
       catch (Exception ex)
       {
-        _logger.LogWarning(ex, "Failed to create checklist item for User with ID: {id}", id);
-        return NotFound("User not found");
+        _logger.LogError(ex, "Unexpected error while creating checklist item for User with ID: {userId}", userId);
+        return StatusCode(500, "An unexpected error occurred.");
       }
     }
   }
