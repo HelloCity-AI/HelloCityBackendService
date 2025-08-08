cdff22d93d45a70fdda4b7d1abd7195c774e75e4
Paul Zhang <hzhang526@gmail.com>
2025-08-09 00:19:32 +1000

    Test(backend): [SCRUM-78] Write to IRepository/IService, Repository/Service for Checklist Items

--- HelloCity.Repository/ChecklistItemRepository.cs
+++ HelloCity.Repository/ChecklistItemRepository.cs
@@ -1,6 +1,7 @@
 using HelloCity.IRepository;
 using HelloCity.Models;
 using HelloCity.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace HelloCity.Repository{
   public class ChecklistItemRepository : IChecklistItemRepository
@@ -9,6 +10,13 @@ namespace HelloCity.Repository{
     public ChecklistItemRepository(AppDbContext context)
     {
       _context = context;
+    }
+    public async Task<List<ChecklistItem>?> GetChecklistItemsAsync(Guid id)
+    {
+      var user = await _context.Users
+        .Include(u => u.ChecklistItems) 
+        .FirstOrDefaultAsync(u => u.UserId == id);
+      return user.ChecklistItems;
     }
     public async Task<ChecklistItem> AddChecklistItemAsync(Guid id, ChecklistItem newChecklistItem)
     {
