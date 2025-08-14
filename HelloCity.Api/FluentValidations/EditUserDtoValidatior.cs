b8383093247064fc4858075b7137a8b4863676e8
WeiliangJia <112872893+WeiliangJia@users.noreply.github.com>
2025-08-14 21:43:32 +0930

    Scrum 92 config fluent validation (#14)
    * chore(backend): [SCRUM-92] install FluentValidation
    
    * feat(backend): [SCRUM-92] Add FluentValidation for CreateUserDto and EditUserDto
    
    * fix(backend): [SCRUM-92] modify CreateUserDtoValidator and EditUserDtoValidator
    
    * fix(backend): [SCRUM-92] modify CreateUserDtoValidator and EditUserDtoValidator
    
    * fix(backend): [SCRUM-92] modify program.cs

---
+++ HelloCity.Api/FluentValidations/EditUserDtoValidatior.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using HelloCity.Api.DTOs.Users;
+using HelloCity.Models.Enums;
+namespace HelloCity.Api.FluentValidations
+{
+    public class EditUserDtoValidator : AbstractValidator<EditUserDto>
+    {
+        public EditUserDtoValidator()
+        {
+            RuleFor(x => x.Username)
+                .MaximumLength(50).WithMessage("Username must be at most 50 characters.")
+                .MinimumLength(1).WithMessage("Username must be at least 1 characters. ");
+
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Email must be valid.")
+                .MaximumLength(100).WithMessage("Email must be at most 100 characters.");
+
+            RuleFor(x => x.Gender)
+                .IsInEnum().WithMessage("Gender is invalid.");
+
+            RuleFor(x => x.PreferredLanguage)
+                .IsInEnum().WithMessage("Preferred language is invalid.");
+
+            RuleFor(x => x.Nationality)
+                .MaximumLength(100).WithMessage("Nationality must be at most 100 characters.")
+                .When(x => !string.IsNullOrWhiteSpace(x.Nationality));
+
+            RuleFor(x => x.City)
+                .MaximumLength(100).WithMessage("City must be at most 100 characters.")
+                .When(x => !string.IsNullOrWhiteSpace(x.City));
+        }
+    }
+}
