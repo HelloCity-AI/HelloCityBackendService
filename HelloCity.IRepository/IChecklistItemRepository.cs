56af4e9374f1968b57ac4d32399c8c51fbb7db12
Andy J <weiliangjia62@gmail.com>
2025-08-18 20:40:24 +0930

    feat(backend): [SCRUM-96] create getSingleChecklistItem in Repository, add Repository Interface

--- HelloCity.IRepository/IChecklistItemRepository.cs
+++ HelloCity.IRepository/IChecklistItemRepository.cs
@@ -1,9 +1,12 @@
 using HelloCity.Models.Entities;
 
-namespace HelloCity.IRepository {
+namespace HelloCity.IRepository
+{
   public interface IChecklistItemRepository
   {
     Task<List<ChecklistItem>?> GetChecklistItemsAsync(Guid userId);
     Task<ChecklistItem> AddChecklistItemAsync(Guid userId, ChecklistItem newChecklistItem);
+    Task<ChecklistItem?> GetSingleChecklistItemAsync(Guid userId, Guid itemId);
+    Task<ChecklistItem> EditChecklistItemAsync(Guid id, ChecklistItem editChecklistItem);
   }
 }
